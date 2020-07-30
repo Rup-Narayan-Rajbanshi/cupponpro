864ec30824360d4aa33e9b73dbb3fd19d085a14b
rashik.vacker360 <rashik.vacker360@gmail.com>
2020-07-30 17:40:18 +0545

    companyinfo api created

--- commonapp/api/company.py
+++ commonapp/api/company.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from commonapp.serializers.company import CompanySerializer
-from commonapp.models import Company
+from commonapp.serializers.company import CompanySerializer, CompanyInfoSerializer
+from commonapp.models.company import Company, CompanyInfo
 from permission import Permission
 
 class CompanyListView(APIView):
@@ -12,5 +12,15 @@ class CompanyListView(APIView):
     def get(self, request):
         company_obj = Company.objects.all()
         serializer = CompanySerializer(company_obj, many=True,\
+            context={"request":request})
+        return Response(serializer.data, status=200)
+
+class CompanyInfoListView(APIView):
+    permission_classes = (Permission ,)
+    serializer_class = CompanyInfoSerializer
+
+    def get(self, request):
+        company_info_obj = CompanyInfo.objects.all()
+        serializer = CompanyInfoSerializer(company_info_obj, many=True,\
             context={"request":request})
         return Response(serializer.data, status=200)
--- commonapp/serializers/company.py
+++ commonapp/serializers/company.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from commonapp.models import Company
+from commonapp.models.company import Company, CompanyInfo
 
 class CompanySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Company
         fields = "__all__"
+
+class CompanyInfoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CompanyInfo
+        fields = "__all__" 
