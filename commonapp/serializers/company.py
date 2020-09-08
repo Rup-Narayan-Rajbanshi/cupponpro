d4a7d2ecd4e4674ce381ed82f78a347becd0978d
Prajesh Shrestha <pjsth666@Kali.Kali>
2020-09-08 20:49:53 +0545

    Apis and Serializers Code Touchup's

--- commonapp/serializers/company.py
+++ commonapp/serializers/company.py
@@ -34,13 +34,9 @@ class CompanySerializer(serializers.ModelSerializer):
             for f in fields_to_exclude:
                 f in self.fields.fields and self.fields.fields.pop(f) or next()
 
+
 class FavouriteCompanySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = FavouriteCompany
         fields = "__all__"
-
-    def exclude_fields(self, fields_to_exclude=None):
-        if isinstance(fields_to_exclude, list):
-            for f in fields_to_exclude:
-                f in self.fields.fields and self.fields.fields.pop(f) or next()
