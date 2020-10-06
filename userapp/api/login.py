11e64729c06afcfb6a536cd39c20e6f67df3f689
Rashik-raj <rashik123.rs@gmail.com>
2020-10-06 12:31:10 +0545

    removed unnecessary code in login api

--- userapp/api/login.py
+++ userapp/api/login.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework_jwt.settings import api_settings
 from userapp.models.user import User, LoginToken
 from userapp.serializers.login import LoginSerializer
 
@@ -11,21 +10,17 @@ class LoginTokenView(generics.GenericAPIView):
 
     def post(self, request, group):
         """
-        An endpoint for login using JWT. Send user, vendor and admin as path parameter for respective login.
+        An endpoint for getting login token. Send user, vendor and admin as path parameter for respective login.
         """
         group_name = {
             'admin': ['admin'],
             'vendor': ['owner', 'manager', 'sales'],
             'user': ['user']
         }
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
         user_obj = User.objects.filter(email=request.data['email'])
         if user_obj:
             if user_obj[0].check_password(request.data['password']):
                 if user_obj[0].group.name in group_name[group]:
-                    payload = jwt_payload_handler(user_obj[0])
-                    token = jwt_encode_handler(payload)
                     login_token_obj = LoginToken.objects.filter(user=user_obj[0].id, is_used=False)
                     # disable all login token of requesting user
                     for obj in login_token_obj:
