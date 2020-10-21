5cce26cda5a2c18a518ea1d6aa3d0cfdbb1e8661
Rashik-raj <rashik123.rs@gmail.com>
2020-10-21 17:01:32 +0545

    menu api created

---
+++ commonapp/serializers/menu.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from commonapp.models.company import Company
+from commonapp.models.product import Product, ProductCategory
+
+class MenuSerializer(serializers.ModelSerializer):
+    menu = serializers.SerializerMethodField()
+    class Meta:
+        model = Company
+        fields = ('id', 'name', 'menu', )
+        # read_only_fields = ('name', )
+
+    def get_menu(self, obj):
+        data = list()
+        product_category_obj = ProductCategory.objects.filter(company=obj.id)
+        for product_category in product_category_obj:
+            menu = dict()
+            menu['category_name'] = product_category.name
+            products = list()
+            product_obj = Product.objects.filter(product_category=product_category)
+            for each_product in product_obj:
+                product = dict()
+                product['id'] = each_product.id
+                product['name'] = each_product.name
+                products.append(product)
+            menu['products'] = products
+            data.append(menu)
+        return data
