669530d2ff0956cdf047b694a1d77fb5d64a0ef6
Rashik-raj <rashik123.rs@gmail.com>
2020-11-20 09:25:02 +0545

    product code and product name added in bill api

--- commonapp/serializers/salesitem.py
+++ commonapp/serializers/salesitem.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 
 class SalesItemSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=False, allow_null=True)
+    product_name = serializers.SerializerMethodField()
+    product_code = serializers.SerializerMethodField()
 
     class Meta:
         model = SalesItem
         fields = '__all__'
+
+    def get_product_name(self, obj):
+        return obj.product.name
+
+    def get_product_code(self, obj):
+        return obj.product.product_code
