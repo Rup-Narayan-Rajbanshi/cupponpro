f73a230a47026bf08dcbcc9f3e500367cae39459
Aakarui <maheshmaharjanc1@gmail.com>
2020-12-22 17:54:29 +0545

    group fixture in master data

--- userapp/management/commands/load_master_data.py
+++ userapp/management/commands/load_master_data.py
@@ -3,6 +3,7 @@ import datetime
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
+from django.contrib.auth.models import Group
 from notifications.models import NotificationCategory
 
 
@@ -14,3 +15,20 @@ class Command(BaseCommand):
             call_command('loaddata', 'notification_category')
         else:
             print('NO NEED TO LOAD NOTIFICATION CATEGORY')
+
+        groups = Group.objects.count()
+        if groups != 5:
+            # new_groups = [
+            #     Group(name='admin'),
+            #     Group(name='owner'),
+            #     Group(name='manager'),
+            #     Group(name='sales'),
+            #     Group(name='user')
+            # ]
+            # Group.objects.bulk_create(new_groups)
+            new_groups = ['admin', 'owner', 'manager', 'sales', 'user']
+            for group in new_groups:
+                g, _ = Group.objects.get_or_create(name=group)
+            print("GROUP LOADED")
+        else:
+            print('NO NEED TO LOAD GROUP')
