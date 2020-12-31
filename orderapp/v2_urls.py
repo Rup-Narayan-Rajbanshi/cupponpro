1866dc6c79e391a20df62412d383510f4f64d15b
Aavash Khatri <aavashkhatri95@gmail.com>
2020-12-31 10:40:13 +0545

    change urls to v2

---
+++ orderapp/v2_urls.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+from django.urls import path
+from rest_framework import routers
+from orderapp.apis.order import OrderCountAPI, TableOrderAPI, TableOrderStatusAPI
+from orderapp.apis.order_line import OrderLineAPI
+from orderapp.apis.table import TableListAPI
+
+
+router = routers.SimpleRouter()
+router.register(r"table-change-status", TableOrderStatusAPI)
+router.register(r"table-order", TableOrderAPI)
+router.register(r"order-line", OrderLineAPI)
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path('order-count', OrderCountAPI.as_view(), name='order_count'),
+    path('tables', TableListAPI.as_view({'get': 'list'}), name='order_count'),
+]
