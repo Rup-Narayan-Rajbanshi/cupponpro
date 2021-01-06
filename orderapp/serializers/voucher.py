11fbb5f22c50c65398b03e95bcd2c7dcf148f003
Aavash Khatri <aavashkhatri95@gmail.com>
2021-01-06 20:18:58 +0545

    coupon fixes

--- orderapp/serializers/voucher.py
+++ orderapp/serializers/voucher.py
@@ -1,8 +1,15 @@
-from commonapp.models.coupon import Voucher
+from rest_framework import serializers
+
+from commonapp.models.coupon import Voucher, Coupon
 from helpers.serializer import CustomModelSerializer
+from helpers.serializer_fields import DetailRelatedField
+from userapp.models import User
 
 
 class VoucherListSerializer(CustomModelSerializer):
+    coupon = DetailRelatedField(model=Coupon, lookup='id', representation='to_representation')
+    user = DetailRelatedField(model=User, lookup='id', representation='to_representation')
+    used_date = serializers.DateTimeField(required=False)
 
     class Meta:
         model = Voucher
