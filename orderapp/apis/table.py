1588f8d61cf52bbea5ad6d00115ce24d7a66eecd
Aavash Khatri <aavashkhatri95@gmail.com>
2021-01-12 16:03:09 +0545

    fix

--- orderapp/apis/table.py
+++ orderapp/apis/table.py
@@ -24,7 +24,9 @@ class AssetFilter(filters.FilterSet):
         if order_status == 'ACTIVE':
             qs = qs.filter(orders__status__in=[ORDER_STATUS['NEW_ORDER'], ORDER_STATUS['CONFIRMED']])
         elif order_status == 'PENDING_PAYMENT':
-            qs = qs.filter(orders__status__in=[ORDER_STATUS['PROCESSING'], ORDER_STATUS['BILLABLE']])
+            qs = qs.filter(orders__status__in=[
+                ORDER_STATUS['NEW_ORDER'], ORDER_STATUS['CONFIRMED'],
+                ORDER_STATUS['PROCESSING'], ORDER_STATUS['BILLABLE']])
             pending = []
             for asset in qs:
                 latest_order = asset.orders.order_by('-created_at').first()
@@ -32,7 +34,7 @@ class AssetFilter(filters.FilterSet):
                     status=ORDER_LINE_STATUS['SERVED']
                 ).count() == latest_order.lines.count():
                     pending.append(asset.id)
-                qs = qs.filter(id__in=pending)
+            qs = qs.filter(id__in=pending)
         return qs.distinct()
 
 
