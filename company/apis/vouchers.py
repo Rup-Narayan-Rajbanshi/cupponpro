0c5efeea43380fdf64f217767dc95e356bbe3347
Aavash Khatri <aavashkhatri95@gmail.com>
2021-01-17 13:59:00 +0545

    validation fix

--- company/apis/vouchers.py
+++ company/apis/vouchers.py
@@ -2,6 +2,7 @@ from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
+from commonapp.models.company import Company
 from commonapp.models.coupon import Voucher
 from commonapp.serializers.coupon import VoucherSerializer
 from helpers.paginations import FPagination
@@ -14,5 +15,5 @@ class UserVoucherListAPI(mixins.ListModelMixin, GenericViewSet):
     pagination_class = FPagination
 
     def get_queryset(self):
-        company_id = self.request.GET.get('company_id')
+        company_id = self.kwargs.get('company_id')
         return self.queryset.filter(user=self.request.user, is_redeem=False, coupon__company__id=company_id)
