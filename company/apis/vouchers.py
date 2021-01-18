59e6038cf9302a1c566a1b15a3781d692110d71d
Aavash Khatri <aavashkhatri95@gmail.com>
2021-01-18 12:56:38 +0545

    user vouchers by phone number

--- company/apis/vouchers.py
+++ company/apis/vouchers.py
@@ -1,5 +1,5 @@
 from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import GenericViewSet
 
 from commonapp.models.company import Company
@@ -10,10 +10,19 @@ from helpers.paginations import FPagination
 
 class UserVoucherListAPI(mixins.ListModelMixin, GenericViewSet):
     queryset = Voucher.objects.all().order_by('-created_at')
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (AllowAny, )
     serializer_class = VoucherSerializer
     pagination_class = FPagination
 
     def get_queryset(self):
         company_id = self.kwargs.get('company_id')
-        return self.queryset.filter(user=self.request.user, is_redeem=False, coupon__company__id=company_id)
+        if self.request.user.is_authenticated:
+            return self.queryset.filter(user=self.request.user, is_redeem=False, coupon__company__id=company_id)
+        else:
+            phone_number = self.request.GET.get('phone_number')
+            if phone_number:
+                return self.queryset.none()
+            else:
+                return self.queryset.filter(user__phone_number=phone_number,
+                                            is_redeem=False,
+                                            coupon__company__id=company_id)
