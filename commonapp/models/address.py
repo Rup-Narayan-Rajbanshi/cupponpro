714ef852959514b64347519111039960bb54710d
sarojdhl <louisaroj69@gmail.com>
2021-01-22 16:59:13 +0545

    address model required

--- commonapp/models/address.py
+++ commonapp/models/address.py
@@ -4,7 +4,7 @@ class Address(models.Model):
     country = models.CharField(max_length=30, null=True, blank=True)
     state = models.CharField(max_length=30, null=True, blank=True)
     city = models.CharField(max_length=30, null=True, blank=True)
-    address = models.CharField(max_length=30, null=True, blank=True)
+    address = models.CharField(max_length=30, null=False, blank=False)
     zip_code = models.CharField(max_length=30, null=True, blank=True)
 
     class Meta:
