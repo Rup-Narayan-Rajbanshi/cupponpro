424bfa67a8222be6bfbc316f8b8ef2e4369b66b8
Aakarui <maheshmaharjanc1@gmail.com>
2021-02-04 13:23:49 +0545

    migrations merged

--- commonapp/migrations/0029_auto_20210204_1315.py
+++ commonapp/migrations/0029_auto_20210204_1315.py
@@ -14,10 +14,5 @@ class Migration(migrations.Migration):
             model_name='productcategory',
             name='position',
             field=models.PositiveIntegerField(blank=True, default=0),
-        ),
-        migrations.AddField(
-            model_name='productcategory',
-            name='sub_type',
-            field=models.CharField(choices=[('VEG', 'VEG'), ('NON-VEG', 'NON-VEG')], default='', max_length=8, null=True),
-        ),
+        )
     ]
