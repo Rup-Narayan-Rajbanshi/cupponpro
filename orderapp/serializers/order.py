60e4abc9df9fb7211243061d90a49d5571ec517b
sarojdhl <louisaroj69@gmail.com>
2021-02-07 17:32:17 +0545

    customer user

--- orderapp/serializers/order.py
+++ orderapp/serializers/order.py
@@ -103,7 +103,7 @@ class CompanyTableOrderSerializer(CustomModelSerializer):
 
     class Meta:
         model = Orders
-        fields = ('id', 'status', 'voucher', 'asset', 'order_lines', 'price_details', 'created_at', 'modified_at', 'user_name', 'phone_number', 'email', 'address')
+        fields = ('id', 'status', 'voucher', 'asset', 'order_lines', 'price_details', 'created_at', 'modified_at', 'user_name', 'phone_number', 'email', 'address','customer','user')
         read_only_fields = ('user', 'customer')
         write_only_fields = ('user_name','phone_number','email','address')
 
