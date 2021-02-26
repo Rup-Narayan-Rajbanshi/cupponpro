47ba528420f117a9a8eb74b5192ac61ac4a8e003
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-02-26 16:49:33 +0545

    like api developed

--- company/v2_urls.py
+++ company/v2_urls.py
@@ -4,12 +4,15 @@ from rest_framework import routers
 from company.apis.company import FavouriteCompanyUserAPI, UserFavouriteCompanyAPI, LocalBusinessAPI
 from company.apis.partner import PartnerAPI
 from company.apis.vouchers import UserVoucherListAPI
+from company.apis.like import CompanyLikeAPI
 
 router = routers.SimpleRouter()
+router.register(r"like", CompanyLikeAPI)
 
 urlpatterns = router.urls
 
 urlpatterns += [
     path('<company_id>/user-vouchers/', UserVoucherListAPI.as_view({'get': 'list'}), name='user_vouchers'),
+
 
 ]
