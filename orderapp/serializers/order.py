c6a31b0579b42c71521284c74bc5aebe7abc19da
sarojdhl <louisaroj69@gmail.com>
2021-03-16 17:20:15 +0545

    commit for all test

--- orderapp/serializers/order.py
+++ orderapp/serializers/order.py
@@ -215,7 +215,7 @@ class CompanyTableOrderSerializer(CustomModelSerializer):
             status = line.get('status', 'NEW')
             product = line['product']
             if str(product.company.id) != str(company.id):
-                raise ValidationError({'detail': '{0} {1} {2} not found.'.format(company.id, product.company.id, product.id)})
+                raise ValidationError({'detail': '{0} not found.'.format(product.name)})
             # if not status == ORDER_LINE_STATUS['CANCELLED']:
             order_line = OrderLines(order=order,
                                     product=product,
