0b6f7110a41e109e43f152c22f18ca1a4e012646
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-04-02 16:04:27 +0545

    get images for company added while quering local-business

--- company/serializers/company.py
+++ company/serializers/company.py
@@ -2,12 +2,15 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from helpers.serializer import CustomModelSerializer, CustomBaseSerializer
 from helpers.serializer_fields import DetailRelatedField
+from django.contrib.contenttypes.models import ContentType
 from commonapp.serializers.company import CompanySerializer
 from helpers.constants import DISCOUNT_TYPE
 from company.models.company import Company, FavouriteCompany
 from commonapp.models.category import Category, SubCategory
 from productapp.models.coupon import Coupon
 from userapp.models import User
+from commonapp.models.image import Image
+from commonapp.serializers.image import ImageDetailSerializer
 
 
 class FavouriteCompanySerializer(CustomModelSerializer):
@@ -22,6 +25,7 @@ class FavouriteCompanySerializer(CustomModelSerializer):
 class LocalBusinessSerializer(CompanySerializer):
     author = DetailRelatedField(model=User, lookup='id', representation='to_representation', read_only=True)
     discount = serializers.SerializerMethodField()
+    images = serializers.SerializerMethodField()
 
     class Meta:
         model = Company
@@ -33,3 +37,9 @@ class LocalBusinessSerializer(CompanySerializer):
         if not coupon:
             coupon = Coupon.objects.filter(company=obj, discount_type=DISCOUNT_TYPE['FLAT']).order_by('-discount').first()
         return coupon.to_representation() if coupon else 0
+
+    def get_images(self,obj):
+        image_obj = Image.objects.filter(object_id=obj.id, content_type=ContentType.objects.filter(model='company').first().id)
+        serializer = ImageDetailSerializer(image_obj, many=True)
+        return serializer.data
+       
